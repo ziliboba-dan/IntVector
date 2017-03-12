0001f6ca9f4b546ea6e52c9e4458837e1a911381
danil <EvtushenkoAleksei@yandex.ru>
2017-03-12 13:39:34 +0700

    Finish

---
+++ src/IntVector.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "IntVector.h"
+
+IntVector *int_vector_new(size_t initial_capacity)
+{
+	IntVector *v = malloc(sizeof(IntVector));
+	if (v == NULL) {
+		return NULL;
+	}
+	v->data = malloc(initial_capacity * sizeof(int));
+	if (v->data == NULL) {
+		free(v);
+		return NULL;
+	}
+	v->size = 0;
+	v->capacity = initial_capacity;
+	return v;
+}
+
+IntVector *int_vector_copy(const IntVector *v)
+{
+	IntVector *c = malloc(sizeof(IntVector));
+	if (c == NULL) {
+		return NULL;
+	}
+	c->data = malloc(v->capacity * sizeof(int));
+	if (c->data == NULL) {
+		free(c);
+		return NULL;
+	}
+	memcpy(c->data, v->data,v->capacity * sizeof(int)); 
+	c->size = v->size;
+	c->capacity = v->capacity;
+	return c;
+}
+
+void int_vector_free(IntVector *v)
+{	
+	free(v->data);
+	free(v);
+	
+}
+
+int int_vector_get_item(const IntVector *v, size_t index)
+{
+	if ((index < v->capacity) && (index >= 0)) {
+		return v->data[index];
+	}
+	return 0;
+	
+}
+
+void int_vector_set_item(IntVector *v, size_t index, int item)
+{
+	if ((index < v->capacity) && (index >= 0)) {
+		v->data[index] = item;
+		v->size++;
+	}
+}
+
+size_t int_vector_get_size(const IntVector *v)
+{
+
+	return v->size;
+}
+ 
+size_t int_vector_get_capacity(const IntVector *v)
+{
+
+	return v->capacity;
+}
+
+int int_vector_push_back(IntVector *v, int item)
+{	
+	if (v->capacity == 0) {
+		v->capacity = 1;
+	}
+	if (v->size < v->capacity) {
+			v->data[v->size] = item;
+			v->size++;
+		}
+	else {
+		v->capacity = v->capacity * 2;
+		v->data = realloc(v->data, v->capacity * sizeof(int));
+		if (v->data == NULL) {
+			return -1;
+		}
+		v->data[v->size] = item;
+		v->size++;
+	}
+	return 0;
+}
+
+void int_vector_pop_back(IntVector *v)
+{	
+	if (v->size > 0) {
+		v->size--;
+		v->data[v->size] = 0;	
+	}
+}
+
+int int_vector_shrink_to_fit(IntVector *v)
+{
+	v->data = realloc(v->data, v->size * sizeof(int));
+	if (v->data == NULL) {
+			return -1;
+	}
+	v->capacity = v->size;
+	return 0;
+}
+
+int int_vector_resize(IntVector *v, size_t new_size)
+{
+	if (v->size <= new_size) {
+		int *tmp = realloc(v->data, new_size * sizeof(int));
+		if (tmp == NULL) {
+			return -1;
+		}
+		v->data = tmp;
+		for (int i = v->size; i < new_size; i++) {
+			v->data[i] = 0;
+		}
+		v->size = new_size;
+	}
+	return 0;
+
+}
+
+
+int int_vector_reserve(IntVector *v, size_t new_capacity)
+{
+	if (new_capacity > v->capacity) {
+		int *tmp = realloc(v->data, new_capacity * sizeof(int));
+		if (tmp == NULL) {
+			return -1;
+		}
+		v->data = tmp;	
+		v->capacity = new_capacity;
+	}
+	return 0;
+}
---
+++ src/main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include "IntVector.h"
+
+void printv(IntVector *v) {
+	for(int i = 0; i < v->size; i++) {
+		printf("%d ", v->data[i]);
+	}
+	printf("\n");
+}
+
+void size_capacity(IntVector *v) {
+	printf("Size = %zu\n", int_vector_get_size(v));
+	printf("Capacity = %zu\n", int_vector_get_capacity(v));
+}
+int main(){
+
+	IntVector *v = int_vector_new(3);
+	size_capacity(v);
+
+	IntVector *c = int_vector_copy(v);
+	size_capacity(c);
+
+	int_vector_free(c);
+
+	//int_vector_get_item
+	printf("Get item: %d\n", int_vector_get_item(v, 0));
+
+	//int_vector_set_item
+	int_vector_set_item(v, 0, 5);
+	printv(v);
+	size_capacity(v);
+	int_vector_set_item(v, 1, 6);
+	printv(v);
+	size_capacity(v);
+
+	//int_vector_push_back
+	if (int_vector_push_back(v, 7) == 0) {
+			printf("Элемент успешно добавлен\n");
+	}
+	else {
+		printf("Error\n");
+	}
+	printv(v);
+	size_capacity(v);
+
+	//int_vector_pop_back
+	int_vector_pop_back(v);
+	printv(v);
+	size_capacity(v);
+	
+	//int_vector_shrink_to_fit
+	if (int_vector_shrink_to_fit(v) == -1) {
+		printf("Error\n");
+	}
+	else {
+		printf("Успешное изменение емкости\n");
+	}
+	size_capacity(v);
+	printv(v);
+	
+	//int_vector_resize
+	if (int_vector_resize(v, 3) == -1) {
+		printf("Ошибка изменения размера массива\n");
+	}
+	else {
+		printf("Успешное изменение размера массива\n");
+	}
+	size_capacity(v);
+	printv(v);
+
+	//int_vector_reserve
+	if (int_vector_reserve(v, 9) == -1) {
+		printf("Ошибка в увеличении емкости\n");
+	}
+	else {
+		printf("Успешное увеличение емкости\n");
+	}
+	size_capacity(v);
+	printv(v);
+
+	//int_vector_push_back
+	if (int_vector_push_back(v, 7) == 0) 
+			printf("Элемент успешно добавлен\n");
+	else printf("Error\n");
+	printv(v);
+	size_capacity(v);
+	int_vector_free(v);
+	return 0;
+}
